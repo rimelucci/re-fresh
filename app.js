7f339b24283254877cf5409379155e73be1ccdf1
Rick Melucci <rimelucci@gmail.com>
2015-12-21 15:01:27 -0500

    Have basic styles for home page

---
+++ app.js
@@ -0,0 +1,15 @@
+$("#top").mouseenter(function(){
+        $("#top").animate({opacity: '0.8'})
+});
+
+$("#top").mouseleave(function(){
+        $("#top").animate({opacity: '1'})
+});
+
+$("#bottom").mouseenter(function(){
+        $("#bottom").animate({opacity: '0.8'})
+});
+
+$("#bottom").mouseleave(function(){
+        $("#bottom").animate({opacity: '1'})
+});
