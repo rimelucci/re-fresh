5e73c634a4099d1ba13493076f64d21ee8a3dd75
Rick Melucci <rimelucci@gmail.com>
2016-01-14 19:18:44 -0500

    added sliding side bar for cart

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 var main = function () {
-
+    var onOrOff = false;
     console.log("hello");
     $('#item-view').hide();
     $('.view-cover').hide();
@@ -13,56 +13,17 @@ var main = function () {
       $('#item-view').fadeOut();
       $('.view-cover').fadeOut();
     });
-    /*$(".friends-button").click(
-        function(){
 
-            $('.menu').animate({
-                right: "0px"
-            }, 300);
-
-
-            $('body').animate({
-                right: "250px"
-            }, 300);
-        }
-    );
-
-    $(".close").click(
-        function(){
-
-            $('.menu').animate({
-                right: "-250px"
-            }, 300);
-
-
-            $('body').animate({
-                right: "0px"
-            }, 300);
-        }
-    );
-
-    $(".startwriting").click(
-        function(){
-
-            $('.startwriting').hide(300);
-            $('.fullform').show(300);
-        }
-    );
-
-    $(".showcomments").click(
-        function(){
-
-            $('.comments').toggle(300);
-        }
-    );
-
-    $(".startcomment").click(
-        function(){
-
-            $('.postcomment').show(300);
-            $('.comments').show(300);
-        }
-    );*/
+    $("#shopping-cart").click(function(){
+      onOrOff = !onOrOff;
+      $("#cart").animate({width: 'toggle'});
+      if (onOrOff) {
+        $("#big-container").animate({left: '-25%'});
+      }
+      else {
+        $("#big-container").animate({left: '0'});
+      }
+    });
 
 }
 
